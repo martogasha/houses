09437e1b42c7b3801d99c0f2a6ad54c52225fdaa
martogasha <m.gachanga97@gmail.com>
2022-03-02 10:50:58 +0300

    first comt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1092,7 +1092,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-9 col-md-8">
-                    <h3 class="text-white xs-text-center my-20 font-400">Are you looking for a House cleaning services?</h3>
+                    <h3 class="text-white xs-text-center my-20 font-400">Are you looking for House cleaning services?</h3>
                 </div>
                 <div class="col-lg-3 col-md-4">
                     <a href="#" class="btn btn-white y-center position-relative d-table xs-mx-auto ms-auto">Order Now</a>
