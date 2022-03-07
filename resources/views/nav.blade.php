3a4e098dfea2477c55ffd5ed36752a9feed669cf
martogasha <m.gachanga97@gmail.com>
2022-03-07 11:48:56 +0300

    firstfdfddgghgh

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg nav-secondary nav-primary-hover nav-line-active">
-    <a class="navbar-brand" href="#"><img class="nav-logo" src="{{url(asset('assets/images/logo/ELIN.png'))}}" alt="Image not found !"></a>
+    <a class="navbar-brand" href="{{url('/')}}"><img class="nav-logo" src="{{url(asset('assets/images/logo/ELIN.png'))}}" alt="Image not found !"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon flaticon-menu flat-small text-primary"></span>
     </button>
